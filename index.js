a79b650df229c78c67d7d6fdba7ebf9e843ec989
kxrn <72281712+kxrn@users.noreply.github.com>
2021-10-05 20:57:52 -0500

    im going to sleep fixing error in morning

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const os = require('os');
+
+
+console.log("════════════════════════")
+console.log("   Platform: " + os.platform() + ' ' + os.arch());
+console.log("   CPU: " + os.cpus())
+console.log("══════════════════════════")
